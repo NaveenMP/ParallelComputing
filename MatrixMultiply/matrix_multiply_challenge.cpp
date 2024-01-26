4ed81acd171940874def5f4389ee6d8d057ea647
Naveen Periasamy <naveenmperiasamy@gmail.com>
2024-01-26 17:26:52 -0500

    Some typical parallelism and concurrancy problems

--- MatrixMultiply/matrix_multiply_challenge.cpp
+++ MatrixMultiply/matrix_multiply_challenge.cpp
@@ -1,8 +1,13 @@
 /**
- * Challenge: Multiply two matrices
+ * Solution: Multiply two matrices
  */
+
 #include <thread>
 #include <cmath>
+#include <cstdio>
+#include <future>
+#include <valarray>
+#include <vector>
 
 /* sequential implementation of matrix multiply */
 void sequential_matrix_multiply(long ** A, size_t num_rows_a, size_t num_cols_a,
@@ -18,13 +23,95 @@ void sequential_matrix_multiply(long ** A, size_t num_rows_a, size_t num_cols_a,
 	}
 }
 
-/* parallel implementation of matrix multiply */
+/* prototype of helper function for parallel_matrix_multiply */
+void parallel_worker(long **, size_t, size_t, long **, size_t, size_t,long **, size_t, size_t);
+
+/* parallel implementation of matrix multiply using std::thread */
+/*
 void parallel_matrix_multiply(long ** A, size_t num_rows_a, size_t num_cols_a,
                               long ** B, size_t num_rows_b, size_t num_cols_b,
 							  long ** C) {
-    /***********************
-     * YOUR CODE GOES HERE *
-     ***********************/
+    size_t num_workers = std::thread::hardware_concurrency();
+    size_t chunk_size = ceil((float)num_rows_a/num_workers);
+
+    std::thread workers[num_workers];
+    for (size_t i=0; i<num_workers; i++) {
+        size_t start_row_c = std::min(i * chunk_size, num_rows_a);
+        size_t end_row_c = std::min((i + 1) * chunk_size, num_rows_a);
+        workers[i] = std::thread(parallel_worker, A, num_rows_a, num_cols_a,
+												  B, num_rows_b, num_cols_b,
+												  C, start_row_c, end_row_c);
+    }
+    for (auto &w : workers) {
+        w.join();
+    }
+}
+*/
+
+/* parallel implementation of matrix multiply using std::async */
+void parallel_matrix_multiply(long ** A, size_t num_rows_a, size_t num_cols_a,
+                              long ** B, size_t num_rows_b, size_t num_cols_b,
+                              long ** C) {
+    size_t num_workers = std::thread::hardware_concurrency();
+    size_t chunk_size = ceil((float)num_rows_a/num_workers);
+
+    std::vector<std::future<void>> futures;
+
+    for (size_t i=0; i<num_workers; i++) {
+        size_t start_row_c = std::min(i * chunk_size, num_rows_a);
+        size_t end_row_c = std::min((i + 1) * chunk_size, num_rows_a);
+        futures.push_back(std::async(std::launch::async, parallel_worker, A, num_rows_a, num_cols_a,
+                                                        B, num_rows_b, num_cols_b,
+                                                        C, start_row_c, end_row_c));
+    }
+
+    // Wait for all futures to finish
+    for (auto& future : futures) {
+        future.wait();
+    }
+}
+
+/* helper function for parallel_matrix_multiply */
+/*
+void parallel_worker(long ** A, size_t num_rows_a, size_t num_cols_a,
+                     long ** B, size_t num_rows_b, size_t num_cols_b,
+					 long ** C, size_t start_row_c, size_t end_row_c) {
+    for (size_t i=start_row_c; i<end_row_c; i++) { // subset of rows in A
+        for (size_t j=0; j<num_cols_b; j++) {
+			C[i][j] = 0; // initialize result cell to zero
+            for (size_t k=0; k<num_cols_a; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            } 
+		}
+	}		
+}
+*/
+
+/* helper function for parallel_matrix_multiply using valarray */
+void parallel_worker(long ** A, size_t num_rows_a, size_t num_cols_a,
+                     long ** B, size_t num_rows_b, size_t num_cols_b,
+					 long ** C, size_t start_row_c, size_t end_row_c) {
+
+
+        // Extract all columns of row 3 from array A into a valarray
+    std::valarray<long> a_row(num_cols_a);
+    std::valarray<long> b_col(num_rows_b);
+    std::valarray<long> c_prod(num_cols_a);
+
+    for (size_t i=start_row_c; i<end_row_c; i++) { // subset of rows in A
+        for (size_t j=0; j<num_cols_b; j++) {
+
+            // Extract all columns of row i from array A into a valarray
+            std::copy(&A[i][0], &A[i][0] + num_cols_a, std::begin(a_row));
+
+            // Extract all rows of column j from array B into a valarray
+            //std::copy(&B[0][j], &B[0][j] + num_rows_b, std::begin(b_col));
+            for (size_t rowIdx = 0; rowIdx < num_rows_b; ++rowIdx) { b_col[rowIdx] = B[rowIdx][j]; }
+            
+            c_prod = a_row * b_col;
+            C[i][j] = c_prod.sum();
+		}
+	}		
 }
 
 int main() {
